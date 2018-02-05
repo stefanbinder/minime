23a5739de82dafcfcf546eda013c61431459742d
Stefan Binder <stefan.binder89@gmail.com>
2018-02-05 13:16:15 +0100

    Added WSC.sol and new structure

--- contracts/lib/SampleCampaign-TokenController.sol
+++ contracts/lib/SampleCampaign-TokenController.sol
@@ -61,17 +61,17 @@ contract Campaign is TokenController, Owned {
     MiniMeToken public tokenContract;   // The new token for this Campaign
     address public vaultAddress;        // The address to hold the funds donated
 
-/// @notice 'Campaign()' initiates the Campaign by setting its funding
-/// parameters
-/// @dev There are several checks to make sure the parameters are acceptable
-/// @param _startFundingTime The UNIX time that the Campaign will be able to
-/// start receiving funds
-/// @param _endFundingTime The UNIX time that the Campaign will stop being able
-/// to receive funds
-/// @param _maximumFunding In wei, the Maximum amount that the Campaign can
-/// receive (currently the max is set at 10,000 ETH for the beta)
-/// @param _vaultAddress The address that will store the donated funds
-/// @param _tokenAddress Address of the token contract this contract controls
+    /// @notice 'Campaign()' initiates the Campaign by setting its funding
+    /// parameters
+    /// @dev There are several checks to make sure the parameters are acceptable
+    /// @param _startFundingTime The UNIX time that the Campaign will be able to
+    /// start receiving funds
+    /// @param _endFundingTime The UNIX time that the Campaign will stop being able
+    /// to receive funds
+    /// @param _maximumFunding In wei, the Maximum amount that the Campaign can
+    /// receive (currently the max is set at 10,000 ETH for the beta)
+    /// @param _vaultAddress The address that will store the donated funds
+    /// @param _tokenAddress Address of the token contract this contract controls
 
     function Campaign(
         uint _startFundingTime,
@@ -79,7 +79,6 @@ contract Campaign is TokenController, Owned {
         uint _maximumFunding,
         address _vaultAddress,
         address _tokenAddress
-
     ) {
         require ((_endFundingTime >= now) &&           // Cannot end in the past
             (_endFundingTime > _startFundingTime) &&
@@ -92,10 +91,10 @@ contract Campaign is TokenController, Owned {
         vaultAddress = _vaultAddress;
     }
 
-/// @dev The fallback function is called when ether is sent to the contract, it
-/// simply calls `doPayment()` with the address that sent the ether as the
-/// `_owner`. Payable is a required solidity modifier for functions to receive
-/// ether, without this modifier functions will throw if ether is sent to them
+    /// @dev The fallback function is called when ether is sent to the contract, it
+    /// simply calls `doPayment()` with the address that sent the ether as the
+    /// `_owner`. Payable is a required solidity modifier for functions to receive
+    /// ether, without this modifier functions will throw if ether is sent to them
 
     function ()  payable {
         doPayment(msg.sender);
@@ -105,31 +104,30 @@ contract Campaign is TokenController, Owned {
 // TokenController interface
 /////////////////
 
-/// @notice `proxyPayment()` allows the caller to send ether to the Campaign and
-/// have the tokens created in an address of their choosing
-/// @param _owner The address that will hold the newly created tokens
-
+    /// @notice `proxyPayment()` allows the caller to send ether to the Campaign and
+    /// have the tokens created in an address of their choosing
+    /// @param _owner The address that will hold the newly created tokens
     function proxyPayment(address _owner) payable returns(bool) {
         doPayment(_owner);
         return true;
     }
 
-/// @notice Notifies the controller about a transfer, for this Campaign all
-///  transfers are allowed by default and no extra notifications are needed
-/// @param _from The origin of the transfer
-/// @param _to The destination of the transfer
-/// @param _amount The amount of the transfer
-/// @return False if the controller does not authorize the transfer
+    /// @notice Notifies the controller about a transfer, for this Campaign all
+    ///  transfers are allowed by default and no extra notifications are needed
+    /// @param _from The origin of the transfer
+    /// @param _to The destination of the transfer
+    /// @param _amount The amount of the transfer
+    /// @return False if the controller does not authorize the transfer
     function onTransfer(address _from, address _to, uint _amount) returns(bool) {
         return true;
     }
 
-/// @notice Notifies the controller about an approval, for this Campaign all
-///  approvals are allowed by default and no extra notifications are needed
-/// @param _owner The address that calls `approve()`
-/// @param _spender The spender in the `approve()` call
-/// @param _amount The amount in the `approve()` call
-/// @return False if the controller does not authorize the approval
+    /// @notice Notifies the controller about an approval, for this Campaign all
+    ///  approvals are allowed by default and no extra notifications are needed
+    /// @param _owner The address that calls `approve()`
+    /// @param _spender The spender in the `approve()` call
+    /// @param _amount The amount in the `approve()` call
+    /// @return False if the controller does not authorize the approval
     function onApprove(address _owner, address _spender, uint _amount)
         returns(bool)
     {
@@ -137,47 +135,45 @@ contract Campaign is TokenController, Owned {
     }
 
 
-/// @dev `doPayment()` is an internal function that sends the ether that this
-///  contract receives to the `vault` and creates tokens in the address of the
-///  `_owner` assuming the Campaign is still accepting funds
-/// @param _owner The address that will hold the newly created tokens
-
+    /// @dev `doPayment()` is an internal function that sends the ether that this
+    ///  contract receives to the `vault` and creates tokens in the address of the
+    ///  `_owner` assuming the Campaign is still accepting funds
+    /// @param _owner The address that will hold the newly created tokens
     function doPayment(address _owner) internal {
 
-// First check that the Campaign is allowed to receive this donation
+        // First check that the Campaign is allowed to receive this donation
         require ((now >= startFundingTime) &&
             (now <= endFundingTime) &&
             (tokenContract.controller() != 0) &&           // Extra check
             (msg.value != 0) &&
             (totalCollected + msg.value <= maximumFunding));
 
-//Track how much the Campaign has collected
+        //Track how much the Campaign has collected
         totalCollected += msg.value;
 
-//Send the ether to the vault
+        //Send the ether to the vault
         require (vaultAddress.send(msg.value));
 
-// Creates an equal amount of tokens as ether sent. The new tokens are created
-//  in the `_owner` address
+        // Creates an equal amount of tokens as ether sent. The new tokens are created
+        //  in the `_owner` address
         require (tokenContract.generateTokens(_owner, msg.value));
 
         return;
     }
 
-/// @notice `finalizeFunding()` ends the Campaign by calling setting the
-///  controller to 0, thereby ending the issuance of new tokens and stopping the
-///  Campaign from receiving more ether
-/// @dev `finalizeFunding()` can only be called after the end of the funding period.
-
+    /// @notice `finalizeFunding()` ends the Campaign by calling setting the
+    ///  controller to 0, thereby ending the issuance of new tokens and stopping the
+    ///  Campaign from receiving more ether
+    /// @dev `finalizeFunding()` can only be called after the end of the funding period.
     function finalizeFunding() {
         require(now >= endFundingTime);
         tokenContract.changeController(0);
     }
 
 
-/// @notice `onlyOwner` changes the location that ether is sent
-/// @param _newVaultAddress The address that will receive the ether sent to this
-///  Campaign
+    /// @notice `onlyOwner` changes the location that ether is sent
+    /// @param _newVaultAddress The address that will receive the ether sent to this
+    ///  Campaign
     function setVault(address _newVaultAddress) onlyOwner {
         vaultAddress = _newVaultAddress;
     }
